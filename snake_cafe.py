38629da655d8b3e36c994d51345ec9a4407b14d7
11mones <mones_leader@hotmail.com>
2023-05-02 23:50:43 +0300

    just created the menu

---
+++ snake_cafe.py
@@ -0,0 +1,36 @@
+print("""
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+
+""")
+      
+Appetizers = ["Wings" , "Cookies" , "Spring Rolls"]
+Entrees =["Salmon" , "Steak" , "Meat Tornado" , "A Literal Garden"]
+Desserts = ["Ice Cream" , "Cake" , "Pie"]
+Drinks = ["Coffee" ,  "Tea" ,  "Unicorn Tears"]
+print("Appetizers")
+print("-------")
+for x in Appetizers:
+  print(x)
+print("\n")
+print("Entrees")
+print("-------")
+for x in Entrees:
+  print(x)
+
+print("\n")
+print("Desserts")
+print("-------")
+for x in Desserts:
+  print(x)
+
+print("\n")
+print("Drinks")
+print("-------")
+for x in Drinks:
+  print(x)
